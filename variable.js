facf483b22d17c64a26a0165f1e5fc313799e747
kimdooh2019 <dopa213@naver.com>
2019-11-25 18:25:24 +0900

    dd

---
+++ variable.js
@@ -0,0 +1,23 @@
+// 1. 선언
+var res;
+// 2. 할당
+res = 10 + 20;
+// 3. 참조
+console.log(res)
+// 4. 재할당
+res = 100
+//console.log(true)
+
+var x = 1
+var y = 2
+
+var temp = 0
+
+console.log("x : " + x + ", y : " + y)
+
+temp = x
+x    = y
+y    = temp
+
+
+console.log("x : " + x + ", y : " + y)
